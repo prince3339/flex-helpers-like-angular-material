f4a41bc7f633049f5de95a8273dc39f1d876c1b9
Sabbir Ahmed Siddiquee <prince3339@ymail.com>
2019-04-27 02:35:54 +0600

    SCSS compilator changed. Now no need to install ruby. Layout generation script updated to avoid same multiple class generation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,22 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
+  const sass = require('node-sass');
+ 
+  require('load-grunt-tasks')(grunt);
+  
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {                              // Task
-      dist: {                            // Target
-        options: {                       // Target options
-          style: 'expanded'
-        },
-        files: {                         // Dictionary of files
-          'style/main.css': 'main.scss'      // 'destination': 'source'
+    sass: {
+      options: {
+        implementation: sass,
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'style/main.css': 'main.scss'
         }
       }
     }
   });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  // Default task(s).
+  
   grunt.registerTask('default', ['sass']);
-
 };
+
